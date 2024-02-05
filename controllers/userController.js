79c4778854603d6ff7a5bef9bfbbe1e63da04feb
Toby <109319639+CyberWolf101@users.noreply.github.com>
2024-02-05 06:56:08 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,39 @@ const createToken = (id) => {                   // we are creating a function so
 
 
 
+
+
+
+//login user
+const login_user = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        // const user = await User.login(email, password)    //the user we returned is stored here so we have assces to it
+        if (!email || !password) {                          
+            throw Error("All fields must be filled")        
+        }
+        const user = await this.findOne({ email })
+        if (!user) {                                                    
+            throw Error("Incorrect email")
+        }                                                        
+        const match = await bcrypt.compare(password, user.password)       
+        if (!match) {                                           
+            throw Error("Incorrect password")
+        }
+       if(match){
+        const token = user._id              //when we get a user we create a token and we store that inside the token const
+        // const token = createToken(user._id)              //when we get a user we create a token and we store that inside the token const
+        res.status(200).json({ email, token, user })
+       }
+       
+    } catch (error) {
+        return res.status(400).json({ error: error.message })
+    }
+
+
+
+}
 //sign-up user
 const singnup_user = async (req, res) => {
     const { email,
@@ -36,26 +69,6 @@ const singnup_user = async (req, res) => {
         return res.status(400).json({ error: error.message })
     }
 }
-
-
-//login user
-const login_user = async (req, res) => {
-    const { email, password } = req.body
-
-    try {
-        const user = await User.login(email, password)    //the user we returned is stored here so we have assces to it
-
-        const token = user._id              //when we get a user we create a token and we store that inside the token const
-        // const token = createToken(user._id)              //when we get a user we create a token and we store that inside the token const
-        res.status(200).json({ email, token, user })
-    } catch (error) {
-        return res.status(400).json({ error: error.message })
-    }
-
-
-
-}
-
 
 module.exports = {
     singnup_user, login_user,
